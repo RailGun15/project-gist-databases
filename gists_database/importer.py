c605181346c57771dbdeafbadddaa3dda21c9bed
Ignacio Keegan <46688572+RailGun15@users.noreply.github.com>
2020-01-21 21:33:43 -0300

    Add files via upload

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,28 @@
 import requests
 
+
 def import_gists_to_database(db, username, commit=True):
-    pass
+    keys = ["id","html_url","git_pull_url","git_push_url",
+            "commits_url","forks_url","public","created_at",
+           "updated_at","comments","comments_url"]
+    
+    url = "https://api.github.com/users/"+username+"/gists"
+    response = requests.get(url)
+    response.raise_for_status()
+    data_list = []
+    for item in response.json():
+        data_dict = {key: val for key,val in item.items() if key in keys}
+        data_list.append(data_dict)
+    cur = db.cursor()
+    for item in data_list:
+        query = """ INSERT INTO gists (forks_url,commits_url,github_id,git_pull_url,git_push_url,html_url,
+        public,created_at,updated_at,comments,comments_url)
+        VALUES (?,?,?,?,?,?,?,?,?,?,?); """
+        vals = tuple(item.values())
+        cur.execute(query,(vals[0],vals[1],vals[2],vals[3],vals[4],vals[5],vals[6],vals[7],
+                           vals[8],vals[9],vals[10]))
+        
+    if commit:
+        db.commit()
+    else:
+        return None
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,24 @@
 from .models import Gist
+from datetime import *
 
 def search_gists(db_connection, **kwargs):
-    pass
+    list_gists = []
+    if kwargs == {}:
+        cursor = db_connection.execute("""SELECT * FROM gists;""")
+        for row in cursor:
+            gist = Gist(row)
+            list_gists.append(gist)
+        return list_gists
+    else:
+        search = [(key,val) for (key,val) in kwargs.items()]
+        for arg in search:
+            if isinstance(arg[1], datetime):
+                cursor = db_connection.execute("SELECT * FROM gists WHERE datetime(created_at) == datetime(:created_at);",(arg[1],))
+            else: 
+                cursor = db_connection.execute("SELECT * FROM gists WHERE {} = ?;".format(arg[0]),(arg[1],))
+            for row in cursor:
+                gist = Gist(row)
+                list_gists.append(gist)
+    return list_gists
+
+        
