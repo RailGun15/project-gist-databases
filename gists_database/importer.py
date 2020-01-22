f3a58ae0694affc23f44e5e52b3effbd04eb7579
Ignacio Keegan <46688572+RailGun15@users.noreply.github.com>
2020-01-21 21:45:06 -0300

    Add files via upload

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -2,25 +2,31 @@ import requests
 
 
 def import_gists_to_database(db, username, commit=True):
-    keys = ["id","html_url","git_pull_url","git_push_url",
-            "commits_url","forks_url","public","created_at",
-           "updated_at","comments","comments_url"]
     
     url = "https://api.github.com/users/"+username+"/gists"
     response = requests.get(url)
     response.raise_for_status()
-    data_list = []
-    for item in response.json():
-        data_dict = {key: val for key,val in item.items() if key in keys}
-        data_list.append(data_dict)
     cur = db.cursor()
-    for item in data_list:
-        query = """ INSERT INTO gists (forks_url,commits_url,github_id,git_pull_url,git_push_url,html_url,
-        public,created_at,updated_at,comments,comments_url)
-        VALUES (?,?,?,?,?,?,?,?,?,?,?); """
-        vals = tuple(item.values())
-        cur.execute(query,(vals[0],vals[1],vals[2],vals[3],vals[4],vals[5],vals[6],vals[7],
-                           vals[8],vals[9],vals[10]))
+    for item in response.json():
+        parameters = {
+            "github_id": item['id'],
+            "html_url": item['html_url'],
+            "git_pull_url": item['git_pull_url'],
+            "git_push_url": item['git_push_url'],
+            "commits_url": item['commits_url'],
+            "forks_url": item['forks_url'],
+            "public": item['public'],
+            "created_at": item['created_at'],
+            "updated_at": item['updated_at'],
+            "comments": item['comments'],
+            "comments_url": item['comments_url'],
+        }
+        query = 
+        """ INSERT INTO gists (github_id, html_url, git_pull_url, git_push_url, commits_url,
+                forks_url, public, created_at,updated_at, comments, comments_url)
+            VALUES (:github_id, :html_url, :git_pull_url, :git_push_url, :commits_url, :forks_url,
+                :public, :created_at, :updated_at,:comments, :comments_url); """
+        cur.execute(query,parameters)
         
     if commit:
         db.commit()
